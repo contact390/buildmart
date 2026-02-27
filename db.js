283b7eeb2561f32eace2eb9e1d8a7353824a408e
contact390 <contact@hitaishicorp.com>
2026-02-27 16:46:54 +0530

    meassage

--- db.js
+++ db.js
@@ -1,20 +1,25 @@
-// db.js
+
 const mysql = require('mysql2');
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: 'localhost',
   user: 'buildmarts',
   password: '2001', // ← replace with your DB password
   database: 'buildimarts',
-  port:3306 // ← make sure this DB exists
+  port:3306, // ← make sure this DB exists
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
-db.connect((err) => {
+// Test connection
+db.getConnection((err, connection) => {
   if (err) {
-    console.error('❌ MySQL connection failed:', err);
-  } else {
-    console.log('✅ MySQL connected.');
+    console.error('❌ DB connection error:', err);
+    return;
   }
+  console.log('✅ buildmart DB connected');
+  connection.release();
 });
 
 module.exports = db;
